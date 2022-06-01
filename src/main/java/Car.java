b2d5fa6e6e286bc55b1dfebc10aa9d15aad96415
Daniel <Daniel200825@wp.pl>
2022-06-01 13:48:38 +0200

    Initial commit

---
+++ src/main/java/Car.java
@@ -0,0 +1,12 @@
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class Car {
+    private String name;
+    private BigDecimal cost;
+    private String fuelType;
+    private BigDecimal fuelCost;
+    private BigDecimal averageFuelConsumption;
+}
---
+++ src/main/java/Constants.java
@@ -0,0 +1,15 @@
+import lombok.Getter;
+
+import java.math.BigDecimal;
+
+@Getter
+public class Constants {
+    private BigDecimal startPrice = BigDecimal.valueOf(8);
+    private BigDecimal pricePerKilometer = BigDecimal.valueOf(3);
+    private BigDecimal employeeSalary = BigDecimal.valueOf(3000);
+    private BigDecimal employerCost = BigDecimal.valueOf(4705.88);
+    private Integer averageSpeed = 30;
+    private Integer averageDistanceToClient = 2;
+    private Integer averageCourseLength = 3;
+    private Integer workPercentage = 70;
+}
